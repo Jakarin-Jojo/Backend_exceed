1da475617499f69c0baf1f6e3f06d077c9a3243a
khosit2 wattanakulwat <khosit.w@ku.th>
2022-02-06 17:35:57 +0700

    complete get name resevertion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from msilib.schema import tables
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from pydantic import BaseModel
+import uvicorn
+
 
 class Reservation(BaseModel):
     name : str
@@ -9,19 +12,25 @@ class Reservation(BaseModel):
     
 client = MongoClient('mongodb://localhost', 27017)
 
-# TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurants"]
 
-# TODO fill in collection name
-collection = db["<put your collection name>"]
+
+collection = db["reservation"]
 
 app = FastAPI()
 
 
-# TODO complete all endpoint.
+
+
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    result = collection.find_one({"name": name}, {"_id":0, "time":1, "table_number": 1})
+    if result != None:
+        return {
+            "result": result 
+        }
+    else:
+        raise HTTPException(404, f"I can't find anyone who reserved this name.")
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
@@ -38,4 +47,3 @@ def update_reservation(reservation: Reservation):
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
     pass
-
